e54161fdfad6b97879926255d431d8d5335bdaf8
neil <chanchunwing6706@gmail.com>
2025-06-17 02:57:01 +0800

    second save

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+class CreateIO
+{
+    public:
+        void print(std::string s)
+        {
+            std::cout<< s;
+        }
+        void println(std::string s)
+        {
+            std::cout<< s <<"\n";
+        }
+};
+
 int main()
 {
-    std::cout<<"Hello World!\n";
+    CreateIO IO;
+    IO.print("Hello World!");
+    IO.print("Hello World!");
+    IO.println("Hello World!");
+    IO.print("Hello World!");
     return 0;
 }
